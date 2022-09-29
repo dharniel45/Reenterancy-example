0cdf7b9ca5dfac3432c6930647dbf97d83c1cb42
Daniel Idowu <74189249+dharniel45@users.noreply.github.com>
2022-09-29 15:17:18 +0100

    Update Re-enterancy.sol

--- Re-enterancy.sol
+++ Re-enterancy.sol
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-// The contract module that facilitates in preventing reentrant calls to a function.
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
 contract ReEntrancyGuard {
     bool internal locked;
-
+// Prevents reentrant calls to a function.
     modifier noReentrant() {
         require(!locked, "No re-entrancy");
         locked = true;
