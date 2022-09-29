a04bbe36a491748b30f8d603a50342a298fefda3
Daniel Idowu <74189249+dharniel45@users.noreply.github.com>
2022-09-29 14:40:49 +0100

    Update re-enterancy.sol

--- re-enterancy.sol
+++ re-enterancy.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
 contract ReEntrancyGuard {
     bool internal locked;
 
